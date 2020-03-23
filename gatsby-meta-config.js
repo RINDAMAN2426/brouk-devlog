017bd75530698cafb028e8d3e4404c70d14a617f
brouk <cdbrouk@gmail.com>
2020-03-23 15:39:28 +0900

    Edit metaconfig

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Dev Blog`,
   author: `brouk`,
   introduction: `web developer`,
-  siteUrl: `https://brouk-blog.netlify.com`, // Your blog site url
+  siteUrl: `https://brouk-devlog.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `cdbrouk`, // Your GitHub account
@@ -15,7 +15,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'cdbrouk/brouk-devlog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
